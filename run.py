ce2ee6ab4aee973f7e568b181b9dcd46a93dac7f
elton.santos <seltons1@gmail.com>
2024-07-29 11:02:55 -0300

    Code improvement.

--- run.py
+++ run.py
@@ -2,10 +2,6 @@ from models.connection_options.connection import Connection
 
 
 connection = Connection()
-
-print(connection.get_db_connection())
-
 connection.connect()
-
-print(connection.get_db_connection())
-
+conn = connection.get_db_connection()
+collection = conn.get_collection("myCollection")
